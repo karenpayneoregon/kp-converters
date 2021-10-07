cd2b2593505e3d65995ad0114821b473f34c4ebc
Karen Payne <Karen.1.Payne@oregon.gov>
2021-10-07 09:49:54 -0700

    added test

--- CoreUnitTestProject/SequenceTest.cs
+++ CoreUnitTestProject/SequenceTest.cs
@@ -36,6 +36,30 @@ namespace CoreUnitTestProject
                 array.SequenceFindMissing().Cast<int>().ToList());
 
         }
+
+
+        /// <summary>
+        /// Same as above with slightly larger array
+        /// </summary>
+        [TestMethod]
+        [TestTraits(Trait.Sequences)]
+        public void SequenceFindMissingIntegersTest1()
+        {
+
+            int[] array =
+            {
+                1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 14, 15, 16, 17, 18, 19, 20, 21, 23, 24, 25,
+                26, 27, 28, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 46, 47, 48,
+                49, 50
+            };
+
+            List<int> expected = new() { 13, 22, 29, 45 };
+
+            CollectionAssert.AreEqual(
+                expected,
+                array.SequenceFindMissing().Cast<int>().ToList());
+
+        }
         /// <summary>
         /// Validate SequenceFindMissing extension
         /// </summary>
